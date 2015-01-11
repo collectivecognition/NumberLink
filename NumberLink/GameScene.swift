6c0503feee7c264165ced6c929beac5c643b4a8f
Will Blanchette <will@unata.com>
2015-01-11 12:24:35 -0500

    Stubbed out basic touch handling

--- NumberLink/GameScene.swift
+++ NumberLink/GameScene.swift
@@ -9,6 +9,11 @@
 import SpriteKit
 
 class GameScene: SKScene {
+    let frameWidth: CGFloat = 0, frameHeight: CGFloat = 0
+    let padding: CGFloat = 20.0
+    let tileSize: CGFloat = 0
+    let startPosition: CGPoint = CGPoint(x: 0.0, y: 0.0)
+    
     required init(coder aCoder: NSCoder) {
         fatalError("NSCoder not supported")
     }
@@ -18,15 +23,14 @@ class GameScene: SKScene {
         
         anchorPoint = CGPoint(x: 0, y: 0)
         
-        let frameWidth = frame.size.width
-        let frameHeight = frame.size.height
-        let padding = CGFloat(20.0)
+        frameWidth = frame.size.width
+        frameHeight = frame.size.height
         
-        let tileSize = CGFloat(min(
+        tileSize = CGFloat(min(
             (frameWidth - (padding * 2)) / CGFloat(Game.shared.puzzle.size.width),
             (frameHeight - (padding * 2)) / CGFloat(Game.shared.puzzle.size.height)))
         
-        let startPosition = CGPoint(
+        startPosition = CGPoint(
             x: frameWidth / 2 - CGFloat(Game.shared.puzzle.size.width) * tileSize / 2 + tileSize / 2,
             y: frameHeight / 2 - CGFloat(Game.shared.puzzle.size.height) * tileSize / 2 - tileSize / 2
         )
@@ -58,7 +62,37 @@ class GameScene: SKScene {
         }
     }
     
+    func locationToGridIndexes(location: CGPoint) -> ([Int]) {
+        return []
+    }
+    
     override func update(currentTime: CFTimeInterval) {
         /* Called before each frame is rendered */
+    }
+    
+    override func touchesBegan(touches: NSSet, withEvent event: UIEvent) {
+        super.touchesBegan(touches, withEvent: event)
+        
+        for touch in touches {
+            let location = touch.locationInNode(self)
+            let touchedNode = nodeAtPoint(location)
+            touchedNode.zPosition = 15
+        }
+    }
+    
+    override func touchesMoved(touches: NSSet, withEvent event: UIEvent) {
+        for touch in touches {
+            let location = touch.locationInNode(self)
+            let touchedNode = nodeAtPoint(location)
+            touchedNode.position = location
+        }
+    }
+    
+    override func touchesEnded(touches: NSSet, withEvent event: UIEvent) {
+        for touch in touches {
+            let location = touch.locationInNode(self)
+            let touchedNode = nodeAtPoint(location)
+            touchedNode.zPosition = 0
+        }
     }
 }
